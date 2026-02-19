5155a917aa045b5ad0d87115c0a75747f2253b30
Waled Karim <waledkarim445@gmail.com>
2026-02-19 15:35:08 +0600

    feat: fetch products from FakeStore API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,8 @@
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+
+  console.log(products);
+
   return <h1>Hello World</h1>;
 }
