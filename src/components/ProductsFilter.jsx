54fb5b4b31806a670b9b7d3c54cd327ffb60ffd8
Waled Karim <waledkarim445@gmail.com>
2026-02-21 15:30:33 +0600

    refactor: refactor products filter into its own component

---
+++ src/components/ProductsFilter.jsx
@@ -0,0 +1,66 @@
+import { CircleX, Funnel } from "lucide-react";
+
+export default function ProductsFilter(props) {
+  return (
+    <>
+      <button
+        onClick={() => props.setOpenModal(true)}
+        className="fixed right-5 bottom-5 rounded-full bg-black p-3 border border-gray-300"
+      >
+        <Funnel size={30} color="white" />
+      </button>
+      {props.openModal && (
+        <div className="fixed inset-0 bg-black/50 backdrop-blur-[1px] z-50">
+          <div className="absolute right-0 left-0 top-1/3 mx-auto bg-white rounded-md p-3 max-w-96">
+            <div className="flex justify-end">
+              <button onClick={() => props.setOpenModal(false)}>
+                <CircleX size={30} color="red" />
+              </button>
+            </div>
+            <div className="my-5">
+              <h3 className="my-2">Filter by Category:</h3>
+              <div className="flex gap-2 flex-wrap items-center">
+                {props.categories.map((category, i) => (
+                  <label
+                    key={i}
+                    className="has-checked:bg-black has-checked:text-white p-2 rounded-md border border-gray-200 cursor-pointer"
+                  >
+                    {category.toUpperCase()}
+                    <input
+                      className="hidden"
+                      type="radio"
+                      name="category"
+                      id={category}
+                      value={category}
+                      checked={props.selectedCategory === category}
+                      onChange={(e) =>
+                        props.setSelectedCategory(e.target.value)
+                      }
+                    />
+                  </label>
+                ))}
+              </div>
+            </div>
+            <div className="my-5">
+              <h3 className="my-2">Filter by Price:</h3>
+              <input
+                type="range"
+                min={0}
+                max={props.maxPrice}
+                className="slider"
+                name="price"
+                id="price"
+                onChange={(e) => props.setPrice(e.target.value)}
+              />
+              <p className="text-center font-medium border rounded-md py-1 px-2">
+                <span className="text-base">
+                  {props.price === 0 ? `Select a price` : `BDT ${props.price}`}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { CircleX, Funnel } from "lucide-react";
 import ProductCard from "./ProductCard";
 import { useMemo, useState } from "react";
+import ProductsFilter from "./ProductsFilter";
 
 export default function ProductsList({ products }) {
   const [openModal, setOpenModal] = useState(false);
@@ -42,62 +42,16 @@ export default function ProductsList({ products }) {
           price={product.price}
         />
       ))}
-      <button
-        onClick={() => setOpenModal(true)}
-        className="fixed right-5 bottom-5 rounded-full bg-black p-3 border border-gray-300"
-      >
-        <Funnel size={30} color="white" />
-      </button>
-      {openModal && (
-        <div className="fixed inset-0 bg-black/50 backdrop-blur-[1px] z-50">
-          <div className="absolute right-0 left-0 top-1/3 mx-auto bg-white rounded-md p-3 max-w-96">
-            <div className="flex justify-end">
-              <button onClick={() => setOpenModal(false)}>
-                <CircleX size={30} color="red" />
-              </button>
-            </div>
-            <div className="my-5">
-              <h3 className="my-2">Filter by Category:</h3>
-              <div className="flex gap-2 flex-wrap items-center">
-                {categories.map((category, i) => (
-                  <label
-                    key={i}
-                    className="has-checked:bg-black has-checked:text-white p-2 rounded-md border border-gray-200 cursor-pointer"
-                  >
-                    {category.toUpperCase()}
-                    <input
-                      className="hidden"
-                      type="radio"
-                      name="category"
-                      id={category}
-                      value={category}
-                      checked={selectedCategory === category}
-                      onChange={(e) => setSelectedCategory(e.target.value)}
-                    />
-                  </label>
-                ))}
-              </div>
-            </div>
-            <div className="my-5">
-              <h3 className="my-2">Filter by Price:</h3>
-              <input
-                type="range"
-                min={0}
-                max={maxPrice}
-                className="slider"
-                name="price"
-                id="price"
-                onChange={(e) => setPrice(e.target.value)}
-              />
-              <p className="text-center font-medium border rounded-md py-1 px-2">
-                <span className="text-base">
-                  {price === 0 ? `Select a price` : `BDT ${price}`}
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
+      <ProductsFilter
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        price={price}
+        setPrice={setPrice}
+        maxPrice={maxPrice}
+        categories={categories}
+      />
     </section>
   );
 }
